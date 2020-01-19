3b2754150fa786f714c32a22bc3f24e77b7b2913
Dmitry Novikov <kromez12@gmail.com>
2020-01-19 21:42:37 +0300

    add redux dev tools support

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from '../Header';
 import Error from '../../pages/Error';
 import UserPage from '../../pages/UserPage';
 import IndexPage from '../../pages/IndexPage';
 
-const Layout = ({ handleModal }) => (
+const Layout = () => (
   <>
-    <Header handleModal={handleModal} />
+    <Header />
     <Switch>
       <Route exact path="/user/">
         <Redirect to="/user/skromez" />
       </Route>
-      <Route path="/user/:id">
-        <UserPage handleModal={handleModal} />
-      </Route>
-      <Route exact path="/">
-        <IndexPage />
-      </Route>
-      <Route path={'/*/*' && '/*/'}>
-        <Error />
-      </Route>
+      <Route path="/user/:id" component={UserPage} />
+      <Route exact path="/" component={IndexPage} />
+      <Route path={'/*/*' && '/*/'} component={Error} />
     </Switch>
   </>
 );
--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import modalReducer from './reducers/modalReducer';
 
-const store = createStore(modalReducer);
+const store = createStore(
+  modalReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 export default store;
